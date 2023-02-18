e7997af05ea7e3e5e1ae6383a4fa4a4ea2fd429c
Jack Fitch <55409055+Jack1424@users.noreply.github.com>
2023-02-18 12:15:43 -0500

    added custom status and cleaned up a bit

--- src/main/java/io/github/jack1424/creeperbot/CreeperBot.java
+++ src/main/java/io/github/jack1424/creeperbot/CreeperBot.java
@@ -3,21 +3,24 @@ package io.github.jack1424.creeperbot;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Message;
+import discord4j.core.object.presence.ClientActivity;
+import discord4j.core.object.presence.ClientPresence;
 
 public class CreeperBot {
 	public static void main(String[] args) {
-		final String token = args[0];
-		final DiscordClient client = DiscordClient.create(token);
-		final GatewayDiscordClient gateway = client.login().block();
+		final GatewayDiscordClient gateway = DiscordClient
+				.create(args[0])
+				.gateway()
+				.setInitialPresence(s -> ClientPresence.online(ClientActivity.watching("you")))
+				.login()
+				.block();
 
 		System.out.println("READY!");
 		System.out.println("Currently in: " + gateway.getGuilds().count().block() + " servers");
 
 		gateway.on(MessageCreateEvent.class).subscribe(event -> {
-			final Message message = event.getMessage();
-			if (message.getContent().contains("creeper"))
-				message.getChannel().block().createMessage("Aw man").block();
+			if (event.getMessage().getContent().contains("creeper"))
+				event.getMessage().getChannel().block().createMessage("Aw man").block();
 		});
 
 		gateway.onDisconnect().block();
